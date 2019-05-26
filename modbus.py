e086ee22946b26fbf398e88ecf926c4768e6a97a
BarryHao <h4506899@gmail.com>
2019-05-26 17:32:07 +0800

    crate API: registerRead, registerWrite, sensorON, sensorOFF

--- modbus.py
+++ modbus.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import minimalmodbus
+from builtins import bytes
 import binascii
 import serial
 import time
@@ -15,75 +16,39 @@ class modbus:
         self.instrument.serial.timeout  = 0.05   # seconds
         # self.name = name
 
-    def sensorRead(self, request):
+    def optionalRead(self, request):
         return self.instrument._performCommand(3, request)
 
-    def sensorWrite(self, request):
+    def optionalWrite(self, request):
         return self.instrument._performCommand(6, request)
 
     def sensorON(self, registeraddress):
-        request = "\x00"
-        request += str(registeraddress)
-        request += "\x00\x01"
-        print("iensorONensorONuc",len(request))
-        return self.sensorWrite(request)
+        request = '\x00'
+        request += chr(int(registeraddress))
+        request += '\x00\x01'
+        return self.instrument._performCommand(6, request)
 
     def sensorOFF(self, registeraddress):
         request = '\x00'
-        request += str(registeraddress)
+        request += chr(int(registeraddress))
         request += '\x00\x00'
-        return self.sensorWrite(request)
+        return self.instrument._performCommand(6, request)
 
-    def voltOutput(self, registeraddress, value):
-        request = "\x00"
-        request += registeraddress
-        print("value:",value)
-        value_string = str(hex(value))
+    def registerRead(self, registeraddress):
+        request = '\x00'
+        request += chr(int(registeraddress))
+        request += '\x00\x01'
 
-        if ((len(value_string) - 2)%2) != 0:
-            value_string = value_string[2:len(value_string)]
-            value_string = "0" + value_string
-        else:
-            value_string = value_string[2:len(value_string)]
+        return self.instrument._performCommand(3, request)
 
-        print("iuahc",value_string)
+    def registerWrite(self, registeraddress, value):
+        request = '\x00'
+        request += chr(int(registeraddress))
+        request += chr(int(value/256))
+        request += chr(int(value%256))
 
-
-        for i in value_string:
-            print(i)
-
-        for i in range(int(len(value_string)/2)):
-            print("i:",i)
-            print("ii:", str(int(value_string[i*2:i*2+2])))
-            request = request + str(int(value_string[i*2:i*2+2]))
-
-        print("ienuc",request)
-        return self.sensorWrite(request)
-
-
-
-        # for i in case:
-        #     print("case:",i)
-        #
-        # for i in value_string:
-        #     print(i)
-
-
-# for i in range((len(s) - 2)/2 + 1):
-#     request += "\x"
-#     request += str(s[3:2])
-#
-# (len(s) - 2)%2
-
-
-        # print("iuaijosvsvrijhc", s[3:5])
-
-
-        value = minimalmodbus._numToTwoByteString(value)
-        print("iuahsiuhisc", value)
-        # request += str(hex(value))
-        # print(request)
-        # return self.sensorWrite(request)
+        print("output value:",value)
+        return self.instrument._performCommand(6, request)
 
 def operation():
     port_name = '/dev/tty.usbserial-DN03VH4V'
@@ -91,20 +56,10 @@ def operation():
     box_ID = 99
 
     acct1 = modbus(port_name, baudrate, box_ID)
-    acct1.sensorON('\x07')
-
-    acct1.voltOutput("\x10", 368)
-
-    # print(acct1.instrument.read_register(13,3))
-    # request = '\x00\x07\x00\x00'
-    # request = '\x00'
-    # request += str(registeraddress)
-    # request += '\x00\x01'
-    # return self.sensorWrite(request)
-    # print(acct1.sensorRead(request) == '')
-    #
-    # for i in acct1.sensorRead(request) :
-    #     print(i)
+    # # test{
+    # acct1.sensorOFF(9)
+    # acct1.voltOutput(10, 368)
+    # # }test
 
 def main():
     print("Aqua Botanical Actuator Drive!")
