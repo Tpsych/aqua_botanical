866a4865ba08c8670ebcd87803b3efabd4e08880
BarryHao <h4506899@gmail.com>
2019-05-26 18:36:53 +0800

    creat SensorMonitor class

--- modbus.py
+++ modbus.py
@@ -5,7 +5,15 @@ import binascii
 import serial
 import time
 
-class modbus:
+class sensorAssignment():
+    def __init__(self, ORP_sensor_num, PH_sensor_num, temp_sensor_num, Oxygen_sensor_num, salt_sensor_num):
+        self.ORP = ORP_sensor_num
+        self.PH = PH_sensor_num
+        self.temp = temp_sensor_num
+        self.Oxygen = Oxygen_sensor_num
+        self.salt = salt_sensor_num
+
+class Modbus:
     def __init__(self, port_name, baudrate, box_ID):
         minimalmodbus.BAUDRATE = baudrate
         self.instrument = minimalmodbus.Instrument(port_name, box_ID, mode ='rtu')
@@ -14,7 +22,6 @@ class modbus:
         self.instrument.serial.parity   = serial.PARITY_EVEN
         self.instrument.serial.stopbits = 1
         self.instrument.serial.timeout  = 0.05   # seconds
-        # self.name = name
 
     def optionalRead(self, request):
         return self.instrument._performCommand(3, request)
@@ -38,27 +45,56 @@ class modbus:
         request = '\x00'
         request += chr(int(registeraddress))
         request += '\x00\x01'
-
         return self.instrument._performCommand(3, request)
 
     def registerWrite(self, registeraddress, value):
-        request = '\x00'
-        request += chr(int(registeraddress))
-        request += chr(int(value/256))
-        request += chr(int(value%256))
+        if value > 65535 or value < 0:
+            print("value parameter fault")
 
-        print("output value:",value)
-        return self.instrument._performCommand(6, request)
+        else:
+            request = '\x00'
+            request += chr(int(registeraddress))
+            request += chr(int(value/256))
+            request += chr(int(value%256))
+
+            print("output value:",value)
+            return self.instrument._performCommand(6, request)
+
+class SensorMonitor:
+    def __init__(self, port_name, baudrate, box_ID, sensors_num):
+        self.modbus = Modbus(port_name, baudrate, box_ID)
+        self.ORP_sensors_num = sensors_num.ORP
+        self.PH_sensors_num = sensors_num.PH
+        self.temp_sensors_num = sensors_num.temp
+        self.Oxygen_sensors_num = sensors_num.Oxygen
+        self.salt_sensors_num = sensors_num.salt
+
+    def readORP(self):
+        return self.modbus.registerRead(self.ORP_sensors_num)
+
+    def readPH(self):
+        return self.modbus.registerRead(self.PH_sensors_num)
+
+    def readtemp(self):
+        return self.modbus.registerRead(self.temp_sensors_num)
+
+    def readOxygen(self):
+        return self.modbus.registerRead(self.Oxygen_sensors_num)
+
+    def readSalt(self):
+        return self.modbus.registerRead(self.salt_sensors_num)
 
 def operation():
     port_name = '/dev/tty.usbserial-DN03VH4V'
     baudrate = 9600
     box_ID = 99
+    sensor_num = sensorAssignment(0, 0, 0, 0, 0)
+    monitor1 = SensorMonitor(port_name, baudrate, box_ID, sensor_num)
 
-    acct1 = modbus(port_name, baudrate, box_ID)
     # # test{
     # acct1.sensorOFF(9)
-    # acct1.voltOutput(10, 368)
+    # q = monitor1.modbus.registerWrite(10, 368)
+    # q = monitor1.readSalt()
     # # }test
 
 def main():
